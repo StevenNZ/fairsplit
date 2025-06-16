42c284b545877820b99fbb2321e7b7ebdfc2169d
stevenNZ <listevennz@gmail.com>
2025-06-16 17:34:40 +1200

    feat: initial user model implemented

---
+++ src/main/java/com/fairsplit/model/User.java
@@ -0,0 +1,26 @@
+package com.fairsplit.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String name;
+
+    private String password;
+}
