3e083fd1dbf6b33b700faac2c7063e82e4938e5d
stevenNZ <listevennz@gmail.com>
2025-06-17 23:52:04 +1200

    feat: initialised user repository interface

---
+++ src/main/java/com/fairsplit/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.fairsplit.repository;
+
+import com.fairsplit.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
