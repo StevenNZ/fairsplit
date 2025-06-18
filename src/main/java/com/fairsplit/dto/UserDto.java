77efc5d82e63f540162d16fe4a5f4a6a82573105
stevenNZ <listevennz@gmail.com>
2025-06-18 21:17:00 +1200

    feat: initial user dto implemented

---
+++ src/main/java/com/fairsplit/dto/UserDto.java
@@ -0,0 +1,17 @@
+package com.fairsplit.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+    private UUID id;
+    private String email;
+    private String name;
+    private String password;
+}
