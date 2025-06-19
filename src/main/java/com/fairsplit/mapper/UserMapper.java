112e337e0e106c36a39cd9cd9d5b45c56734acbc
stevenNZ <listevennz@gmail.com>
2025-06-19 19:59:57 +1200

    initialised user mapper class

---
+++ src/main/java/com/fairsplit/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.fairsplit.mapper;
+
+import com.fairsplit.dto.UserDto;
+import com.fairsplit.model.User;
+
+public class UserMapper {
+
+    public static UserDto mapToUserDto(User user) {
+        return new UserDto(user.getId(), user.getEmail(), user.getName(), user.getPassword());
+    }
+
+    public static User mapToUser(UserDto userDto) {
+        return new User(userDto.getId(), userDto.getEmail(), userDto.getName(), userDto.getPassword());
+    }
+}
