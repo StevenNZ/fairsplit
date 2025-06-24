3b2279a6c8b3208502a9a9d89b395f9c6b77a7f0
stevenNZ <listevennz@gmail.com>
2025-06-24 16:45:40 +1200

    feat: expense entity implemented

---
+++ src/main/java/com/fairsplit/model/Expense.java
@@ -0,0 +1,31 @@
+package com.fairsplit.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "expenses")
+public class Expense {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private int amount;
+
+    private String description;
+
+    @Column(name = "local_date", columnDefinition = "DATE")
+    private LocalDate localDate;
+
+    @ManyToOne
+    @Column(name = "user_id")
+    private User user;
+}
