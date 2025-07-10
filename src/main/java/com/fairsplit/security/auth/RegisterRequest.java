06ceaf175091afbbf60434a80ca6df9ba361e4d2
stevenNZ <listevennz@gmail.com>
2025-07-10 20:05:04 +1200

    feat: login and register request dto implemented

---
+++ src/main/java/com/fairsplit/security/auth/RegisterRequest.java
@@ -0,0 +1,14 @@
+package com.fairsplit.security.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+    private String name;
+    private String email;
+    private String password;
+}
