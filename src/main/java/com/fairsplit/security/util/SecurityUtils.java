fe03e3c73bf0ca4d2bac915bb72996128199ec62
stevenNZ <listevennz@gmail.com>
2025-07-14 16:50:10 +1200

    feat: security utils class to check ownership implemented

---
+++ src/main/java/com/fairsplit/security/util/SecurityUtils.java
@@ -0,0 +1,30 @@
+package com.fairsplit.security.util;
+
+import com.fairsplit.model.User;
+import com.fairsplit.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class SecurityUtils {
+
+    private final UserRepository userRepository;
+
+    public User getCurrentUser() {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        return userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    public void checkOwnership(UUID targetUserId) {
+        User currentUser = getCurrentUser();
+        if (!currentUser.getId().equals(targetUserId)) {
+            throw new AccessDeniedException("Unauthorized access");
+        }
+    }
+}
