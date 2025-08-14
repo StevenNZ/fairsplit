2da2befd3525963320d9019a274d98d2d5dbd51e
stevenNZ <listevennz@gmail.com>
2025-08-14 20:13:05 +1200

    feat: group expense model implemented

--- src/main/java/com/fairsplit/model/Group.java
+++ src/main/java/com/fairsplit/model/Group.java
@@ -1,4 +1,34 @@
 package com.fairsplit.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "groups")
 public class Group {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "created_by_user_id")
+    private User createdBy;
+
+    @OneToMany(mappedBy = "group", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<GroupMembership> memberships;
+
+    @OneToMany(mappedBy = "group", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<GroupExpense> expenses;
 }
