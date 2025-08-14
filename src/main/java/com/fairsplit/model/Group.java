79533f34b403186381e6a64219d84fca81a5d1e7
stevenNZ <listevennz@gmail.com>
2025-08-14 20:06:35 +1200

    feat: group membership model implemented

---
+++ src/main/java/com/fairsplit/model/Group.java
@@ -0,0 +1,4 @@
+package com.fairsplit.model;
+
+public class Group {
+}
--- src/main/java/com/fairsplit/model/GroupMembership.java
+++ src/main/java/com/fairsplit/model/GroupMembership.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Data
@@ -17,7 +18,14 @@ public class GroupMembership {
     @GeneratedValue
     private UUID id;
 
-    private String name;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
-    private String password;
+    @ManyToOne
+    @JoinColumn(name = "group_id")
+    private Group group;
+
+    @Column(name = "joined_at", columnDefinition = "DATE")
+    private LocalDate joinedAt;
 }
